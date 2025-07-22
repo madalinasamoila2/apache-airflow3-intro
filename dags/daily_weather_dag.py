cc69ea5f29b36b265a6f00fdf96b8d0179f9f91d
Madalina Samoila <madalinasam25@outlook.com>
2025-07-22 15:42:24 +0100

    Update daily_weather_dag.py

--- dags/daily_weather_dag.py
+++ dags/daily_weather_dag.py
@@ -2,36 +2,35 @@ from airflow import DAG # used to import the DAG class from Apache Airflow
 # DAG is a collection of all the tasks you want to run and every airflow workflow must be wrapped in a DAG object to define the schedule, start date, .. 
 from airflow.providers.standard.operators.python import PythonOperator # let's you run a python function as a task (data extraction, transformation)
 from datetime import datetime, timedelta # datetime is used to define the DAG's start date, timedelta used to define DAG's schedule
-import requests # used to retreive data from API
+import requests # retrieve data from API
 import pandas as pd
 import os
-import mysql.connector
+import mysql.connector # connect to mysql
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import csv
 
  
-# -------- Task: Check API Availability --------
+# Task: is_api_available to check whether the API link is available ------------------------------------------------------------------
 def is_api_available():
     url = "https://api.open-meteo.com/v1/forecast?latitude=51.5072&longitude=0.1276&current=temperature_2m"
     try:
         response = requests.get(url, timeout=10)
         response.raise_for_status()
-        print("✅ API is available.")
+        print(" API is available.")
     except Exception as e:
-        raise RuntimeError(f"❌ API not reachable: {e}")
+        raise RuntimeError(f"API not reachable: {e}")
 
 
-# --------- HOURLY DAG: fetch_weather ---------
-# Fetch weather python function is used as a DAG task to retrieve hourly date from API
+# Task: extract_transform_weather which extracts & transforms weather data from the API, extracted hourly to be stored as csv/mysql -----
 def extract_transform_weather():
     url = "https://api.open-meteo.com/v1/forecast?latitude=51.5072&longitude=0.1276&current=temperature_2m,relative_humidity_2m,wind_speed_10m,rain,precipitation,weather_code&timezone=Europe%2FLondon"
     response = requests.get(url)
     data = response.json()
 
-    # Create dataframe from hourly time and temperature
+    # Create dataframe with necessary fields
     from datetime import datetime
-    import pandas as pd # data manipulation
+    import pandas as pd 
     summary = {
         "date":datetime.now().strftime("%Y-%m-%d"),
         "time": data["current"]["time"],
@@ -43,11 +42,12 @@ def extract_transform_weather():
         "weather_code": data["current"]["weather_code"]
     }
 
-    # ALERT LOGIC (inline)
+    # Add alert message as a new column to determine the weather condition for that hour
     code = int(summary["weather_code"])
     temp = summary["temperature"]
     alert_messages = []
 
+    # Weather codes taken from the open-meteo link
     rain_codes = {51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82}
     snow_codes = {71, 73, 75, 77, 85, 86}
     thunderstorm_codes = {95, 96, 99}
@@ -66,26 +66,25 @@ def extract_transform_weather():
 
     df = pd.DataFrame([summary])
 
-    # Save to local file inside Airflow container
+    # Save to local file inside Airflow container 
     import os
     filename = f"/tmp/hourly_summary_{summary['date']}.csv" # temporarily stores the data in a temporary folder in docker
     if os.path.exists(filename):
-        df.to_csv(filename, mode='a', index=False, header=False)  # Append without header
+        df.to_csv(filename, mode='a', index=False, header=False)  # append without header
         print(f"Saving appended CSV file to: {filename}")
     else:
-        df.to_csv(filename, index=False, header=True)  # Write with header
+        df.to_csv(filename, index=False, header=True)  # write with header
         print(f"Saving CSV file to: {filename}")
 
     # Return small snippet for XCom
     return df.to_dict()
 
-# Summarise daily weather function is used as a DAG task to retrieve hourly date from API
+
+# Task: summarise_daily_weather used as a DAG task to aggregate values and stored them in a daily summary csv
 def summarize_daily_weather():
-    # Use today's date to generate daily report
-    # yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
     today = (datetime.now()).strftime("%Y-%m-%d")
     hourly_file = f"/tmp/hourly_summary_{today}.csv"
-    print(f"Looking for hourly file: {hourly_file}")
+    print(f"Looking for hourly file: {hourly_file}") # printing steps to help me check if the data is being retrieved
     
     if not os.path.exists(hourly_file):
         raise FileNotFoundError(f"No hourly data found for {today}")
@@ -116,7 +115,7 @@ def summarize_daily_weather():
 
     return summary
 
-# Check_extreme_weather function is used as a DAG task to determine the weather condition and take precautions
+# Task: check_extreme_weather used as a DAG task (mainky on Docker)to determine weather conditions hourly and take precautions
 def check_extreme_weather():
     import pandas as pd
     import os
@@ -169,7 +168,7 @@ def check_extreme_weather():
     return alert_messages
 
 
-# -------- Task: Load to MySQL --------
+# Task: load_to_mysql used to load the created hourly_summary & daily_summary and store into MySQL Database for future use.
 def load_to_mysql(**kwargs):
     try: 
         today = datetime.now().strftime("%Y-%m-%d")
@@ -184,7 +183,7 @@ def load_to_mysql(**kwargs):
         df_hourly = pd.read_csv(hourly_file)
         df_summary = pd.read_csv(summary_file)
 
-    # Get database credentials 
+    # Database credentials 
         db_host = "host.docker.internal" # originally is localhost but because we are using Docker, host.docker.internal allow your Docker container (Airflow) to connect to your host machine's MySQL server
         db_user = "root"
         db_password = ''
@@ -195,16 +194,16 @@ def load_to_mysql(**kwargs):
             host=db_host, user=db_user, password=db_password, database=db_database
         )
 
-        # Create a SQLAlchemy engine
+        # Create a SQLAlchemy engine to connect to MySQL database
         engine = create_engine(
             f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_database}"
         )
 
-        # Insert DataFrame into a SQL table, with a name of table_name given
-        df_hourly.to_sql(name="weather_hourly", con=engine, if_exists="append", index=False)
-        df_summary.to_sql(name="weather_summary", con=engine, if_exists="append", index=False)
+    # Insert DataFrame into a SQL table, with a name of table_name given
+        df_hourly.to_sql(name="weather_hourly", con=engine, if_exists="replace", index=False)
+        df_summary.to_sql(name="weather_summary", con=engine, if_exists="replace", index=False)
        
-        # Close the database connection
+    # Close the database connection
         conn.close()
 
     except Exception as error:
@@ -215,8 +214,9 @@ default_args = {
     'retries': 0,
 }
 
-# --------- HOURLY DAG: extract & transform & load data ---------
-# Define an Airflow DAG 'simple_weather_to_csv' which runs the 'fetch_weather' python function hourly. 
+
+# Airflow DAG 'simple_weather_to_csv' which runs the tasks created above using a schedule, start date -------------------------------------
+# # DAG is a collection of all the tasks you want to run and every airflow workflow must be wrapped in a DAG object to define the schedule, start date, .. 
 with DAG(
     dag_id="simple_weather_to_csv", # unique identifier for the DAG
     default_args=default_args,     # applies default parameters
@@ -230,15 +230,14 @@ with DAG(
         task_id="is_api_available",
         python_callable=is_api_available
     )
-    task_extract_transform = PythonOperator( # PythonOperator is an Airflow operator that runs a Python function as a task.
-        task_id="extract_transform_weather", # identifies the specific task in the DAG
-        python_callable=extract_transform_weather # This tells the operator which function to run when this task executes. 
+    task_extract_transform = PythonOperator( 
+        task_id="extract_transform_weather", 
+        python_callable=extract_transform_weather 
     )
     task_summarize = PythonOperator(
         task_id="summarize_weather",
         python_callable=summarize_daily_weather
     )
-
     task_check_extreme_weather = PythonOperator(
         task_id="check_extreme_weather",
         python_callable=check_extreme_weather
@@ -247,7 +246,7 @@ with DAG(
         task_id="load_to_mysql",
         python_callable=load_to_mysql
     )
-
+    # Order in which to run the tasks
     task_check_api >> task_extract_transform >> task_summarize >> task_check_extreme_weather >> task_load_to_mysql
 
 
